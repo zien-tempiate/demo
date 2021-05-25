62bda67f06673a22cc7fae1cfce9cb7344a2dba5
zien-tempiate <84601286+zien-tempiate@users.noreply.github.com>
2021-05-25 23:51:34 +0200

    Update sport.js

--- sport.js
+++ sport.js
@@ -1,4 +1,4 @@
-document.querySelector("#featured-wrapper").style.display= "block";
+document.querySelector("#outer-wrapper").style.display= "block";
 document.querySelector(".footerbar").style.height= "50px";
         document.querySelector("#match-yaster").onclick=function(){
         this.classList.add("open-match");
