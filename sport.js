bff5917ed930f4e792f1d3022425962345aef9e6
zien-tempiate <84601286+zien-tempiate@users.noreply.github.com>
2021-05-25 14:12:48 +0200

    Create sport.js

---
+++ sport.js
@@ -0,0 +1,27 @@
+document.querySelector("#featured-wrapper").style.display= "block";
+document.querySelector(".footerbar").style.height= "50px";
+        document.querySelector("#match-yaster").onclick=function(){
+        this.classList.add("open-match");
+        document.querySelector("#match-today").classList.remove("open-match");
+        document.querySelector("#match-tom").classList.remove("open-match");      
+        document.querySelector("#matchtables-yester").classList.add("openn");
+        document.querySelector("#matchtables-today").classList.remove("openn");
+        document.querySelector("#matchtables-Tom").classList.remove("openn");
+        };
+        document.querySelector("#match-today").onclick=function(){
+        this.classList.add("open-match");
+        document.querySelector("#match-yaster").classList.remove("open-match");
+        document.querySelector("#match-tom").classList.remove("open-match");      
+        document.querySelector("#matchtables-yester").classList.remove("openn");
+        document.querySelector("#matchtables-today").classList.add("openn");
+        document.querySelector("#matchtables-Tom").classList.remove("openn");
+        };
+        document.querySelector("#match-tom").onclick=function(){
+        this.classList.add("open-match");
+        document.querySelector("#match-yaster").classList.remove("open-match");
+        document.querySelector("#match-today").classList.remove("open-match");      
+        document.querySelector("#matchtables-today").classList.remove("openn");
+        document.querySelector("#matchtables-yester").classList.remove("openn");
+        document.querySelector("#matchtables-Tom").classList.add("openn");
+        };
+      }, 700);
